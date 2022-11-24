d148fafc2d808cb5f5f6e40fe48d78298be5c753
muhammadosmanqureshi <115480972+muhammadosmanqureshi@users.noreply.github.com>
2022-11-24 15:56:58 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,179 @@
+//  var a = "karachi";
+
+//  var b = prompt("Enter city name");
+
+
+//   a=a. toLocaleLowerCase();
+//   b=b. toLocaleLowerCase();
+//   a=a. toLocaleUpperCase();
+//   b=b. toLocaleUpperCase();
+
+
+//   if (a==b);{
+//   console.log('same');
+//   }
+
+//     var firsletter = a.slice(0,1)
+//     var otherletter = a.slice(1)
+
+    
+//   a = firsletter. toUpperCase() + otherletter. toLocaleLowerCase();
+//   console.log(a)
+
+//..............................................................
+
+// var a =  "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Earum, saepe voluptatum voluptate dolor, ducimus necessitatibus amet eveniet eum, mollitia illum vitae sapiente. Unde in facere illo, quis dignissimos veritatis voluptatibus!";
+
+// var b = "amet";
+
+// for( var i = 0; i < a.length; i++){
+
+// var word = a.slice(i,i + 4);
+// if (word == b){
+//     console.log("found",word);
+//     console.log(i);
+// var startinglatter = a.slice(0,i);
+// var myword = "ametment";
+// var otherletter = a.slice(i + 4);
+
+// a = startinglatter + myword + otherletter
+// }    
+
+// }
+// console.log(a);
+//.........................................................
+// var a = a.replace (/amet/g , "amemtment")
+
+// console.log(a)
+
+//.....................................................
+
+
+// var a = Maths.round(5.9);
+// var a = Math.floor(1.9);
+// var a = Math.ceil(1.2);
+// var a = 1.342452423423;
+
+// a = a.toFixed(3);
+
+// console.log(a)
+
+// var a = 10;
+//  var b = "20";
+// var type = typeof a; ////// TYPE BATAYEGA NUMBER HAI YA STRING
+// var btype = typeof b;
+
+//  console.log(a);
+//  console.log(type);
+
+//  var a = 10;
+// // a = +a ;
+//a = Number (a);
+//a = parseInt (a);
+//a = parseFloat (a);
+//var type = typeOf a;
+// a = a.toString();
+// console.log(a)
+// ..............................//
+// var a = Math.random()
+//  var toss = Math.round(a)
+
+// console.log(toss)
+// console.loglog(a)
+
+
+// var a = Math.floor(Math.random()*6) +1;
+// console.log(a) 
+
+// var months = [
+//     "jan" ,"fab", "mar", "apri", "may", "june", "july", "aug", "sept","oct", "nov", "dec"
+// ]
+
+
+// var dt = new Date();
+// var b = dt.getMonth();
+// //var a = dt.getDay();
+// //var a = dt.getDate();
+// //var a = dt.getFullYear();
+// //var a = dt.getHours();
+// //var a = dt.getMinutes();
+// //var a = dt.getSeconds();
+// //var a = dt.getMilliseconds();
+
+// console.log(months[b]);
+
+
+// function abc(){
+
+//     alert("hellow fuctions")
+// }
+// abc()
+
+// function  calculatediscount(a,b){
+
+
+//     var amount = avar discount =b                                 ;
+//     var totalamount = amount - discount;
+// console.log(totalamount);
+
+// }
+// calculatediscount(100,20)
+// calculatediscount(200,40)
+// calculatediscount(200,50)
+// calculatediscount(200,30)
+
+// /...............................
+// function abc(){
+// var a = 10;
+// return a;
+
+
+
+// }
+
+
+// var c = abc();
+
+// console.log(c);
+
+
+// var day = 'mon';
+// switch (day) {
+
+//     case "sun":
+//         console.log("holiday");
+//         break;
+//     case "mon":
+//         console.log("first day of week")
+//         break;
+//     case "tues":
+//         console.log("2nd day of week");
+//         break;
+
+//         default:
+//             console.log("no day found")
+// }
+
+
+// var i = 0;
+
+// while(i < 10) {
+// console.log(i)
+// i++;
+// }
+
+
+// var i = 0;
+
+// do {
+// console.log(i)
+// i++;
+// }
+// while (i < 1);
+
+
+// function abc(a){
+//     console.log(a);
+    // alert("rest in peace")
+    
+    
